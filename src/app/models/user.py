25c9d326ea30a65b798a72406363f1277a5e762e
nnnLik <makhmudov_ra@vk.com>
2023-03-02 02:01:12 +0300

    [ADD] autoincr, [EDIT] time field name

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -11,22 +11,23 @@ from sqlalchemy import (
     )
 
 
-class FastFlowUser(Base):
+class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, nullable=False, index=True, unique=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, unique=True)
 
     username = Column(String(50), unique=True, nullable=False)
     email = Column(String(120), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
 
-    about_me = Column(Text)
+    about = Column(Text)
 
-    date_joined = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
     
     is_active = Column(Boolean, default=True)
     is_staff = Column(Boolean, default=False)
     is_superuser = Column(Boolean, default=False)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User: {self.username}>'
