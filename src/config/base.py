41b7c92d2e836c2b862c34382b89f21402328611
nnnLik <makhmudov_ra@vk.com>
2023-03-01 23:40:35 +0300

    [ADD] database connection

---
+++ src/config/base.py
@@ -0,0 +1,2 @@
+from .database import Base
+from src.app.models.user import FastFlowUser
---
+++ src/config/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from .settings import (
+    SQL_ENGINE, 
+    SQL_USER,
+    SQL_PASS,
+    SQL_HOST,
+    SQL_DB,
+    SQL_PORT,
+    )
+
+
+SQLALCHEMY_DATABASE_URL = f"{SQL_ENGINE}://{SQL_USER}:{SQL_PASS}@{SQL_HOST}:{SQL_PORT}/{SQL_DB}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base: DeclarativeMeta = declarative_base()
