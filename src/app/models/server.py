2b19669e8c7539fcad4e3a79e32688d8ea7ea9c8
nnnLik <makhmudov_ra@vk.com>
2023-03-02 02:00:37 +0300

    [ADD] main server model

---
+++ src/app/models/server.py
@@ -0,0 +1,29 @@
+from src.config.database import Base
+
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column, 
+    Integer, 
+    DateTime,
+    ForeignKey,
+    String
+    )
+
+
+class Server(Base):
+    __tablename__ = "server"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, unique=True)
+
+    server_name = Column(String(50), nullable=False)
+
+    created_by = Column(Integer, ForeignKey('user.id'))
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    rel_created_by = relationship("User", foreign_keys=[created_by])
+
+    def __repr__(self):
+        return f'<Server name: {self.server_name}>'
