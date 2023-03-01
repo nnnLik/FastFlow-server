d1b5fec45462838469a90e28d397fa3f91343445
nnnLik <makhmudov_ra@vk.com>
2023-03-02 02:00:16 +0300

    [ADD] server members

---
+++ src/app/models/server_members.py
@@ -0,0 +1,31 @@
+from src.config.database import Base
+
+from src.app.models.user import User
+from src.app.models.server import Server
+
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column, 
+    Integer, 
+    DateTime,
+    ForeignKey,
+    )
+
+
+class ServerMembers(Base):
+    __tablename__ = "server_members"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, unique=True)
+
+    server = Column(Integer, ForeignKey('server.id'))
+    user = Column(Integer, ForeignKey('user.id'))
+
+    joined_at = Column(DateTime(timezone=True), server_default=func.now())
+    left_at = Column(DateTime(timezone=True), nullable=True, default=None)
+
+    rel_user = relationship("User", foreign_keys=[user])
+    rel_server = relationship("Server", foreign_keys=[server])
+
+    def __repr__(self):
+        return f'<ServerMembers user: {self.user} ServerMember: {self.user}>'
