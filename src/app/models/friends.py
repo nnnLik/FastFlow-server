4ab4eb194e16f8c0408410d8fae6b094fb8a8fe9
nnnLik <makhmudov_ra@vk.com>
2023-03-02 01:59:53 +0300

    [ADD] friends model

---
+++ src/app/models/friends.py
@@ -0,0 +1,27 @@
+from src.config.database import Base
+
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column, 
+    Integer, 
+    DateTime,
+    ForeignKey
+    )
+
+
+class Friends(Base):
+    __tablename__ = "friends"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, unique=True)
+
+    user_id_1 = Column(Integer, ForeignKey('user.id'))
+    user_id_2 = Column(Integer, ForeignKey('user.id'))
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    friends = relationship("User", foreign_keys=[user_id_1, user_id_2])
+
+    def __repr__(self):
+        return f'<User_1: {self.user_id_1} - User_2: {self.user_id_2}>'
