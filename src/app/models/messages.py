f3f130e358d3d2d4ea71058c0f78a937e6c8c964
nnnLik <makhmudov_ra@vk.com>
2023-03-02 02:00:04 +0300

    [ADD] messages model

---
+++ src/app/models/messages.py
@@ -0,0 +1,32 @@
+from src.config.database import Base
+
+from src.app.models.user import User
+
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column, 
+    Integer, 
+    DateTime,
+    ForeignKey,
+    Text
+    )
+
+
+class Message(Base):
+    __tablename__ = "message"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, unique=True)
+
+    user = Column(Integer, ForeignKey('user.id'))
+    server_room = Column(Integer, ForeignKey('server_message_room.id'))
+
+    content = Column(Text)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    rel_user = relationship("User", foreign_keys=[user])
+    rel_server_room = relationship("ServerMessageRoom", foreign_keys=[server_room])
+
+    def __repr__(self):
+        return f'<Message name: {self.content}>'
