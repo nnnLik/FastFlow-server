17f6e0c9132618579ac7f9a0dab01475f4cc13d5
nnnLik <makhmudov_ra@vk.com>
2023-03-01 23:41:17 +0300

    [ADD] get env var

---
+++ src/config/settings.py
@@ -0,0 +1,8 @@
+from decouple import config
+
+SQL_ENGINE = config("SQL_ENGINE")
+SQL_USER = config("SQL_USER")
+SQL_PASS = config("SQL_PASS")
+SQL_HOST = config("SQL_HOST")
+SQL_DB = config("SQL_DB")
+SQL_PORT = config("SQL_PORT")
