2973e654793b3f0fc92c66027f31960ec59449d6
nnnLik <makhmudov_ra@vk.com>
2023-03-02 02:01:23 +0300

    [ADD] init models

--- src/config/base.py
+++ src/config/base.py
@@ -1,2 +1,9 @@
 from .database import Base
-from src.app.models.user import FastFlowUser
+
+from src.app.models.user import User
+from src.app.models.friends import Friends
+from src.app.models.server import Server
+from src.app.models.server_members import ServerMembers
+from src.app.models.server_room import ServerMessageRoom
+from src.app.models.messages import Message
+
