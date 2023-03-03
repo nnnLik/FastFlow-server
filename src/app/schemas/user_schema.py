7d99adc028ecd1d9188a0a174bf95f34e5190e59
nnnLik <makhmudov_ra@vk.com>
2023-03-03 04:27:36 +0300

    [FIX] update and create schemas

--- src/app/schemas/user_schema.py
+++ src/app/schemas/user_schema.py
@@ -1,22 +1,39 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
-    id: int
     username: str
     email: str
-    about: str
+    about: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
     created_at: datetime
+    updated_at: datetime
     is_active: bool
     is_staff: bool
     is_superuser: bool
 
+    class Config:
+        orm_mode = True
 
-class UserList(UserBase):
-    id: int
-    username: str
-    email: str
+
+class AllUsers(UserBase):
+    pass
 
     class Config:
         orm_mode = True
+
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+    about: Optional[str] = None
