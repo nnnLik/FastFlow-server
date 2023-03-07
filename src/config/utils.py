159d8daa21174883367caf17e3d8265783be7a57
nnnLik <makhmudov_ra@vk.com>
2023-03-08 00:48:45 +0300

    [EDIT] change func to get db session

--- src/config/utils.py
+++ src/config/utils.py
@@ -1,5 +1,11 @@
 from starlette.requests import Request
 
+from src.config.database import SessionLocal
 
-async def get_db(request: Request):
-    return request.state.db
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI, Request, Response
 
-from src.app.routers import routes
+from fastapi.middleware.cors import CORSMiddleware
 from src.config.database import SessionLocal
+
+from src.app.routers import routes
+
 
 app = FastAPI(title="FastFlow")
 
+origins = ["*"]
 
-@app.middleware("http")
-async def db_session_middleware(request: Request, call_next):
-    response = Response("Internal Server Error", status_code=500)
-    try:
-        request.state.db = SessionLocal()
-        response = await call_next(request)
-    finally:
-        request.state.db.close()
-    return response
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(router=routes)
