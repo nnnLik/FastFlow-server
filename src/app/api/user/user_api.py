59e9453518d43430d795ce1f69de0177ce439456
nnnLik <makhmudov_ra@vk.com>
2023-03-08 00:50:15 +0300

    [EDIT] rename user schemas

--- src/app/api/user/user_api.py
+++ src/app/api/user/user_api.py
@@ -5,52 +5,48 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.config.utils import get_db
-from src.app.schemas.user_schema import User, AllUsers, UserCreate, UserUpdate
+from src.app.schemas.user_schemas import (
+    UserSchema,
+    AllUsersSchema,
+    UserUpdateSchema,
+)
 
 from src.app.api.user.service import (
-    check_for_create_user,
     delete_user_by_id,
     update_user_by_id,
     user_by_email,
     users,
     user_by_username,
     user_by_id,
-    create,
 )
 
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[AllUsers])
+@router.get("/", response_model=List[AllUsersSchema])
 async def all_users(db: Session = Depends(get_db)):
     return users(db)
 
 
-@router.get("/{user_id}", response_model=User)
+@router.get("/{user_id}", response_model=UserSchema)
 async def get_user_by_id(user_id: int, db=Depends(get_db)):
     return user_by_id(db, user_id=user_id)
 
 
-@router.get("/name/{username}", response_model=User)
+@router.get("/name/{username}", response_model=UserSchema)
 async def get_user_by_username(username: str, db: Session = Depends(get_db)):
     return user_by_username(db, username=username)
 
 
-@router.get("/email/{email}", response_model=User)
+@router.get("/email/{email}", response_model=UserSchema)
 async def get_user_by_email(email: str, db: Session = Depends(get_db)):
     return user_by_email(db, email=email)
 
 
-@router.post("/", response_model=User, status_code=201)
-async def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    check_for_create_user(db, user)
-    return create(db=db, user=user)
-
-
-@router.patch("/{user_id}", response_model=User)
+@router.patch("/{user_id}", response_model=UserSchema)
 async def update_user(
-    user_id: int, user_update: UserUpdate, db: Session = Depends(get_db)
+    user_id: int, user_update: UserUpdateSchema, db: Session = Depends(get_db)
 ):
     return update_user_by_id(db, user_update, user_id)
 
