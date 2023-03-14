5d156ce8bd7aba7f4963f2941169a8d1af9cb695
nnnLik <makhmudov_ra@vk.com>
2023-03-14 21:55:48 +0300

    [ADD] server route

--- src/app/routers.py
+++ src/app/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from src.app.api.user.user_api import router as user_api
 from src.app.api.auth.auth_api import router as auth_api
+from src.app.api.server.server_api import router as server_api
 
 routes = APIRouter()
 
@@ -10,6 +11,13 @@ routes.include_router(
     prefix="/user",
     tags=[
         "user",
+    ],
+)
+routes.include_router(
+    server_api,
+    prefix="/server",
+    tags=[
+        "server",
     ],
 )
 routes.include_router(
