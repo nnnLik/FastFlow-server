794207b08facb678c3a9e4cd31f2712760cb8561
nnnLik <makhmudov_ra@vk.com>
2023-03-14 21:57:21 +0300

    [EDIT] update by pep8

--- src/app/api/user/user_api.py
+++ src/app/api/user/user_api.py
@@ -25,33 +25,46 @@ from src.app.api.auth.jwt_bearer import JWTBearer
 router = APIRouter()
 
 
-@router.get("/", dependencies=[Depends(JWTBearer())], response_model=List[AllUsersSchema])
+@router.get(
+    "/", dependencies=[Depends(JWTBearer())], response_model=List[AllUsersSchema]
+)
 async def all_users(db: Session = Depends(get_db)):
     return users(db)
 
 
-@router.get("/{user_id}", dependencies=[Depends(JWTBearer())], response_model=UserSchema)
+@router.get(
+    "/{user_id}", dependencies=[Depends(JWTBearer())], response_model=UserSchema
+)
 async def get_user_by_id(user_id: int, db=Depends(get_db)):
     return user_by_id(db, user_id=user_id)
 
 
-@router.get("/name/{username}", dependencies=[Depends(JWTBearer())], response_model=UserSchema)
+@router.get(
+    "/name/{username}", dependencies=[Depends(JWTBearer())], response_model=UserSchema
+)
 async def get_user_by_username(username: str, db: Session = Depends(get_db)):
     return user_by_username(db, username=username)
 
 
-@router.get("/email/{email}", dependencies=[Depends(JWTBearer())], response_model=UserSchema)
+@router.get(
+    "/email/{email}", dependencies=[Depends(JWTBearer())], response_model=UserSchema
+)
 async def get_user_by_email(email: str, db: Session = Depends(get_db)):
     return user_by_email(db, email=email)
 
 
-@router.patch("/{user_id}", dependencies=[Depends(JWTBearer())], response_model=UserSchema)
+@router.patch(
+    "/{user_id}", dependencies=[Depends(JWTBearer())], response_model=UserSchema
+)
 async def update_user(
     user_id: int, user_update: UserUpdateSchema, db: Session = Depends(get_db)
 ):
     return update_user_by_id(db, user_update, user_id)
 
 
-@router.delete("/{user_id}", dependencies=[Depends(JWTBearer())],)
+@router.delete(
+    "/{user_id}",
+    dependencies=[Depends(JWTBearer())],
+)
 async def delete_user(user_id: int, db: Session = Depends(get_db)):
     return delete_user_by_id(db, user_id)
